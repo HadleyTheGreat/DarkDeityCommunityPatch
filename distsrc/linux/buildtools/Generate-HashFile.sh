0ef281b9e7e9b26e4bba3560ca21d40cfcb47145
HadleyTheGreat <64345365+HadleyTheGreat@users.noreply.github.com>
2026-07-02 01:07:47 -0700

    reorganizing the source folders and associated script changes

--- distsrc/linux/buildtools/Generate-HashFile.sh
+++ distsrc/linux/buildtools/Generate-HashFile.sh
@@ -2,20 +2,31 @@
 
 set -euo pipefail
 
-# Get the directory where the script is located (equivalent to $PSScriptRoot)
-BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if [ "$#" -ne 1 ]; then
+    echo "Error: Missing argument."
+    echo "Usage: $0 {path to folder}"
+    exit 1
+fi
 
+TARGET_DIR="$1"
+
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "Error: '$TARGET_DIR' is not a valid directory."
+    exit 1
+fi
+
+# Get the directory where the script is located
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "BASE_DIR = $BASE_DIR"
 
+GAME_DIR=$($BASE_DIR/Get-SteamAppPath.sh 1374840)
+echo "GAME_DIR = $GAME_DIR"
+
 # Define paths (handling relative paths)
-IMAGES_DIR="$(realpath "$BASE_DIR/../../images")"
-CSV_PATH="$(realpath "$BASE_DIR/../scripts/hashes.csv")"
+CSV_PATH="$(realpath "$TARGET_DIR/hashes.csv")"
 
 # Ensure the destination directory for the CSV exists
 mkdir -p "$(dirname "$CSV_PATH")"
-
-GAME_DIR=$($BASE_DIR/steamappfinder 1374840)
-echo "GAME_DIR = $GAME_DIR"
 
 # Calculate SHA256 hashes
 # 'sha256sum' outputs: "[hash] [file_path]". We use 'awk' to grab just the hash.
